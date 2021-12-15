21f9aa566dec1e4a8329033c66035cf9bb79a830
Andrew Lorber <andrewlorber@aol.com>
2021-12-15 01:00:33 -0500

    Adds colors file

---
+++ frontend/src/colors.ts
@@ -0,0 +1,7 @@
+// Colors used throughout this project
+
+const LIGHT_BLUE = '#EAF6FF';
+const MEDIUM_BLUE = '#B7E0FF';
+const DARK_BLUE = '#2395FF';
+const LIGHT_GREEN = '#E5A5A6';
+const LIGHT_RED = '#A8E28E';
